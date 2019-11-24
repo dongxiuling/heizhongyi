8fdbc641be0a558fff75ca3ea487e01dece94f9a
dongxiuling <644961699@qq.com>
2019-11-24 16:33:40 +0800

    js

--- 网站/index.js
+++ 网站/index.js
@@ -1,5 +1,5 @@
 var oBtn = document.getElementById('list-btn');
-var oSubList = document.getElementsByClassName('sub-list')[0]; //返回数组
+var oSubList = document.getElementsByClassName('sub-list')[0]; //返回数组 取到数组中索引是0的元素
 oBtn.onclick = function(){
     // console.log(oSubList.style.display); //只能取到内联样式
     if(oSubList.style.display == "block"){ //如果显示 ->隐藏
