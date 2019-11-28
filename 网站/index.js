2badcf0640c26b25872b67267c20403ff4a9beb6
dongxiuling <644961699@qq.com>
2019-11-28 15:38:39 +0800

    点击按钮

--- 网站/index.js
+++ 网站/index.js
@@ -11,3 +11,18 @@ oBtn.onclick = function(){
 
 
 
+// 轮播图 
+// 1 给按钮绑定事件 ->  按钮有被激活的样式active
+
+$('.btn-box li').on('click',function(){
+    // 当前点击的li=> $(this)  添加active
+
+    // $(this).addClass('active');
+    // $(this).siblings().removeClass('active');
+    $(this).addClass('active').siblings().removeClass('active');
+})
+
+// $('li').find('span').css('background',red)
+
+
+// $('#div1').next()
